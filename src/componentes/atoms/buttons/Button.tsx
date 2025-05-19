93f53ccd0b731bbd41fd560e9ee75c7b7f0c9d40
Jonas <jbrionestecnico@gmail.com>
2025-05-19 08:42:04 -0400

    modificaciones en el thema

--- src/componentes/atoms/buttons/Button.tsx
+++ src/componentes/atoms/buttons/Button.tsx
@@ -7,12 +7,12 @@ const Button: React.FC = () => {
             <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button className="bg-primary-50 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded">
         Botón Primario
         </button>
         <div className="bg-white dark:bg-neutral-900 text-neutral-900 dark:text-neutral-100">
-  Dark mode ready bro.
-</div>
+            Dark mode ready bro.
+        </div>
         </>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,24 +8,9 @@ module.exports = {
     "./.storybook/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-        colors: {
-            primary: '#1DA1F2',
-            secondary: '#14171A',
-            accent: '#657786',
-            neutral: '#AAB8C2',
-            'base-100': '#FFFFFF',
-            info: '#1DA1F2',
-            success: '#17BF63',
-            warning: '#FFAD1F',
-            error: '#E0245E',
-        },
-        fontFamily: {
-            sans: ['Inter', 'sans-serif'],
-        },
-    },
+    extend: {},
   },
   plugins: [
     require('@tailwindcss/forms')
-],
+  ],
 }
